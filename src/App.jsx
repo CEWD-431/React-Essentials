96bb2e21bc47068bf71afbfa872073b302a84795
KritikaSharmaKS <sh.kritika01@gmail.com>
2024-01-24 22:16:56 -0500

    build first custom component

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,22 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+function CountCard() {
   const [count, setCount] = useState(0)
+  return (
+    <div className="card">
+    <button onClick={() => setCount((count) => count + 1)}>
+      count is {count}
+    </button>
+    <p>
+      Edit <code>src/App.jsx</code> and save to test HMR
+    </p>
+  </div>
+  )
+}
+
+
+function App() {
 
   return (
     <>
@@ -17,14 +31,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+      <CountCard />
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
