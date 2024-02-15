88993f88b7a0cdb5c40b5836be0fba47e5369e8f
KritikaSharmaKS <sh.kritika01@gmail.com>
2024-02-15 13:55:47 -0500

    special children prop - component composition

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import CoreConcepts from './components/CoreConcepts';
+import TabButton from './components/TabButton';
 import DisplayRandomGreetings from './components/DisplayRandomGreetings';
 import {CORE_CONCEPTS} from './components/data';
 import './App.css';
@@ -25,13 +26,24 @@ function App() {
   return (
     <>
       <div>
-        <CoreConcepts title = {CORE_CONCEPTS[0].title} 
-                      description = {CORE_CONCEPTS[0].description} 
-                      image = {CORE_CONCEPTS[0].image} 
-        />
-        <CoreConcepts {...CORE_CONCEPTS[1]} />
-        <CoreConcepts {...CORE_CONCEPTS[2]} />
-        <CoreConcepts {...CORE_CONCEPTS[3]} />
+        <section id = "core-concepts">
+          <CoreConcepts title = {CORE_CONCEPTS[0].title} 
+                        description = {CORE_CONCEPTS[0].description} 
+                        image = {CORE_CONCEPTS[0].image} 
+          />
+          <CoreConcepts {...CORE_CONCEPTS[1]} />
+          <CoreConcepts {...CORE_CONCEPTS[2]} />
+          <CoreConcepts {...CORE_CONCEPTS[3]} />
+        </section>
+        <section id = "examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
+        </section>
       </div>
       <h1>Using Props - Core Concepts</h1>
     </>
---
+++ src/components/TabButton.jsx
@@ -0,0 +1,3 @@
+export default function TabButton (props) {
+    return <div><button>{props.children}</button></div>
+}
