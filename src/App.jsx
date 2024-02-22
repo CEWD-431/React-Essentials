d9c67ff0d9660dc22d24b0d4650d64171442e5f4
KritikaSharmaKS <sh.kritika01@gmail.com>
2024-02-22 12:00:15 -0500

    splitting components by feature and state

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,26 @@
 
 import CoreConcepts from './components/CoreConcepts';
-import TabButton from './components/TabButton';
-import DisplayRandomGreetings from './components/DisplayRandomGreetings';
-import {CORE_CONCEPTS, EXAMPLES} from './components/data';
+import Examples from './components/Examples';
+import { CORE_CONCEPTS } from './components/data';
 import './App.css';
 import { useState } from 'react';
 
-
-function CountCard() {
-  const [count, setCount] = useState(0)
-  return (
-    <div className="card">
-    <button onClick={() => setCount((count) => count + 1)}>
-      count is {count}
-    </button>
-    <p>
-      Edit <code>src/App.jsx</code> and save to test HMR
-    </p>
-  </div>
-  )
-}
+// function CountCard() {
+//   const [count, setCount] = useState(0)
+//   return (
+//     <div className="card">
+//     <button onClick={() => setCount((count) => count + 1)}>
+//       count is {count}
+//     </button>
+//     <p>
+//       Edit <code>src/App.jsx</code> and save to test HMR
+//     </p>
+//   </div>
+//   )
+// }
 
 
 function App() {
-  const [ selectedTopic, setSelectedTopic ] = useState();
-  function selectHandler (selectedButton) {
-    // selectedButton => 'components', 'jsx', 'props', 'state'
-    setSelectedTopic(selectedButton);
-    console.log(selectedTopic);
-  }
-
-  let tabContent = 'Please click a button';
-  if (selectedTopic) {
-    tabContent =  <div id="tab-content">
-                    <h3>{EXAMPLES[selectedTopic].title}</h3>
-                    <p>{EXAMPLES[selectedTopic].description}</p>
-                    <pre>
-                      <code>{EXAMPLES[selectedTopic].code}</code>
-                    </pre>
-                  </div>
-  }
-
   return (
     <>
       <div>
@@ -48,16 +28,7 @@ function App() {
           <h2>Core Concepts</h2>
           {CORE_CONCEPTS.map((coreConceptItem) =>  <CoreConcepts key={coreConceptItem.title} {...coreConceptItem} />)}
         </section>
-        <section id = "examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton isSelected={selectedTopic==="components"} onSelect={() => selectHandler('components')}>Components</TabButton>
-            <TabButton isSelected={selectedTopic==="jsx"} onSelect={() => selectHandler('jsx')}>JSX</TabButton>
-            <TabButton isSelected={selectedTopic==="props"} onSelect={() => selectHandler('props')}>Props</TabButton>
-            <TabButton isSelected={selectedTopic==="state"} onSelect={() => selectHandler('state')}>State</TabButton>
-          </menu>
-          {tabContent}
-        </section>
+        <Examples/>
       </div>
       <h1>Basic React App</h1>
     </>
