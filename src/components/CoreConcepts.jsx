5051a0773aab662096b91a05e7657916e0e97c33
KritikaSharmaKS <sh.kritika01@gmail.com>
2024-02-22 07:38:17 -0500

    using fragment to wrap and return children elements

--- src/components/CoreConcepts.jsx
+++ src/components/CoreConcepts.jsx
@@ -1,10 +1,12 @@
+import { Fragment } from 'react';
+
 export default function CoreConcepts ({image, title, description}) {
     return (
-      <div>
+      <Fragment>
         <img src={image} alt={title} width="200" height="200" />
         <h3>{title}</h3>
         <p>{description}</p>
-      </div>
+      </Fragment>
     );
   }
   
