6468da236b4ffff93cd07fe9fa12b07a8094c438
KritikaSharmaKS <sh.kritika01@gmail.com>
2024-02-22 07:39:07 -0500

    you don't have to use fragment for modern apps

--- src/components/CoreConcepts.jsx
+++ src/components/CoreConcepts.jsx
@@ -1,12 +1,10 @@
-import { Fragment } from 'react';
-
 export default function CoreConcepts ({image, title, description}) {
     return (
-      <Fragment>
+      <>
         <img src={image} alt={title} width="200" height="200" />
         <h3>{title}</h3>
         <p>{description}</p>
-      </Fragment>
+      </>
     );
   }
   
